9165540c9ce97dbdd689adc5f16505837432c578
cWetaski <charles@wetaski.com>
2024-09-06 10:33:25 +0200

    Added spectralBandDerivativeFunction (for future use of rosseland approximation)

--- src/spectralBandDerivative.m
+++ src/spectralBandDerivative.m
@@ -1,23 +1,25 @@
+% ©2024 ETH Zurich, Charles Wetaski, Sebastian Sas Brunser, Emiliano Casati
 %   AUTHOR: Charles Wetaski
-%   LAST CHECKED: 2024-06-07 (Charles Wetaski)
+%   LAST CHECKED: 2024-09-06 (Charles Wetaski)
 
-function dEdT = spectralBandDerivative(lambda1,lambda2,T,nn,dT)
+function dEdT = spectralBandDerivative(lambda1,lambda2,T,dT)
     % SPECTRALBANDDERIVATIVE
     % Computes derivative (with respect to temperature) of spectral blackbody emissive power between two specified
-    % wavelengths in um at a given T and refractive index nn
+    % wavelengths in um at a given T
     % Inputs:
     %   lambda1 (scalar double) [um]:   Lower wavelength of spectral band
     %   lambda2 (scalar double) [um]:   Upper wavelength of spectral band (lambda1 <= lambda2)
     %   T (double) [K]:                 Temperature in kelvin. Can be scalar or an array of any dimension, output will have
     %                                   same shape as T.
-    %   nn (double) [-]:                Refractive index of medium (or medium the surface is emitting into) in the band
-    %                                   Can be scalar or array in same dimension of T
+    % (optional)
+    %   dT (double) [K]:                Temperature difference over which to evaluate the derivative, otherwise determined automatically.
+    %
     % Outputs:
     %   dE_b_band: [W]:                  Blackbody emissive power in the specified wavelength band
     
     %% Constants
-    if nargin == 4
-        dT = T*sqrt(eps);
+    if nargin == 3
+        dT = T*sqrt(eps); % eps here is machine epsilon, apparently this optimal due to some floating point stuff!
         dT(T==0) = sqrt(eps);
     end
     f_l = spectralBandPower(lambda1,lambda2,T,nn);
