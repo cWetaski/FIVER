f672e4dafddc66196859a884ebae0f1e13f9e6a8
cWetaski <charles@wetaski.com>
2024-09-06 10:34:21 +0200

    Refractive index is no longer an argument, since we assume properties are in terms of vacuum wavelength

--- src/spectralBandPower.m
+++ src/spectralBandPower.m
@@ -1,17 +1,19 @@
+% ©2024 ETH Zurich, Charles Wetaski, Sebastian Sas Brunser, Emiliano Casati
 %   AUTHOR: Charles Wetaski
-%   LAST CHECKED: 2024-06-07 (Charles Wetaski)
-
-function E_b_band = spectralBandPower(lambda1,lambda2,T,nn)
+%   LAST CHECKED: 2024-09-06 (Charles Wetaski)
+%   
+function E_b_band = spectralBandPower(lambda1,lambda2,T)
     % SPECTRALBANDPOWER
     % Integrates spectral blackbody emissive power between two specified
-    % wavelengths in um for a given T and refractive index nn
+    % wavelengths in um for a given T for refractive index = 1 (vacuum)
+    % We only consider the vacuum emissive power since spectral properties of real materials are typically reported
+    % with respect to the vacuum wavelength, not the wavelength within the medium. Increases in the spectral band power
+    % due to a nonunit refractive index (i.e., multiplication by n^2) must applied outside of this function.
     % Inputs:
     %   lambda1 (scalar double) [um]:   Lower wavelength of spectral band
     %   lambda2 (scalar double) [um]:   Upper wavelength of spectral band (lambda1 <= lambda2)
     %   T (double) [K]:                 Temperature in kelvin. Can be scalar or an array of any dimension, output will have
     %                                   same shape as T.
-    %   nn (double) [-]:                Refractive index of medium (or medium the surface is emitting into) in the band
-    %                                   Can be scalar or array in same dimension of T
     % Outputs:
     %   E_b_band: [W]:                  Blackbody emissive power in the specified wavelength band
     
@@ -24,7 +26,7 @@ function E_b_band = spectralBandPower(lambda1,lambda2,T,nn)
     
     
     %% Planck's Law of EM Radiation [W/(m^2 um)]
-    f = @(lambda_m) (2*pi*h*c^2)./(nn.^2*lambda_m.^5.*(exp((h*c)./(nn.*lambda_m.*Kb.*T))-1)); % [W/(m^2-m)]
+    f = @(lambda_m) (2*pi*h*c^2)./(lambda_m.^5.*(exp((h*c)./(lambda_m.*Kb.*T))-1)); % [W/(m^2-m)]
     E_b_band = integral(f,lambda1_m,lambda2_m,'ArrayValued',true);
 end
 
