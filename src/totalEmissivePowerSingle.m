53a399ba56fdac62a2539b1ac92c694938cb899e
cWetaski <charles@wetaski.com>
2024-09-06 10:35:45 +0200

    Copyright notice

--- src/totalEmissivePowerSingle.m
+++ src/totalEmissivePowerSingle.m
@@ -1,31 +1,34 @@
-function Q_emit = totalEmissivePowerSingle(lambdas,voxel_spaces,T,i)
-    %TOTALEMISSIVEPOWER Get total emissive power for voxel space with varying spectral properties
-    %   Detailed explanation goes here
+% ©2024 ETH Zurich, Charles Wetaski, Sebastian Sas Brunser, Emiliano Casati
+%   AUTHOR: Charles Wetaski
+%   LAST CHECKED: 2024-09-06 (Charles Wetaski)
+% 
+
+function Q_emit = totalEmissivePowerSingle(spectral_band_edges,voxel_spaces,T,ind)
+    %TOTALEMISSIVEPOWERSINGLE Get total emissive power at a single voxel in a spectral voxel space at a given temperature
+    %   inputs:
+    %       spectral_band_edges (1D double) [um]:   Edges of spectral bands (in increasing wavelength)
+    %       voxel_spaces (1D cell):                 Cell array of voxel_space objects in each spectral band
+    %       T (scalar double)    [K]:               Temperature to evaluate the emissive power at
+    %       ind (scalar double (int)):              Linear index of the voxel of interest.
+    %   outputs:
+    %       Q_emit (scalar) [W]: Total emissive power of the voxel  
 Q_emit = 0;
 if T == 0
     return;
 end
-N_bands = length(lambdas)+1;
+N_bands = length(spectral_band_edges)-1;
 vx_scale = voxel_spaces{1}.voxel_scale;
 for n = 1:N_bands
-    nn = voxel_spaces{n}.refractive_indexes(i);
-    if n == 1
-        lb = 0;
-        ub = lambdas(n);
-    elseif n == N_bands
-        lb = lambdas(n-1);
-        ub = 10^12/T/nn; % in case nan, just set it to 10^12
-    else
-        lb = lambdas(n-1);
-        ub = lambdas(n);
-    end
-    PM_kappa = voxel_spaces{n}.PM_absorption_coeffs(i);
+    nn = voxel_spaces{n}.refractive_indexes(ind);
+    lb = spectral_band_edges(n-1);
+    ub = spectral_band_edges(n);
+    PM_kappa = voxel_spaces{n}.PM_absorption_coeffs(ind);
     if PM_kappa == 0
-        surf_area = voxel_spaces{n}.surface_areas(i);
+        surf_area = voxel_spaces{n}.surface_areas(ind);
         if surf_area == 0
             continue
         else
-            opaq_eps = voxel_spaces{n}.opaque_emissivities(i);
+            opaq_eps = voxel_spaces{n}.opaque_emissivities(ind);
             if opaq_eps == 0
                 continue;
             else
